3369c123b689a07d96fc9150d4b2fb9e80e2f719
jestin <156066672+jestin-16@users.noreply.github.com>
2024-07-04 14:40:18 +0530

    Add files via upload

---
+++ project/user/book.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>user booking page</p>
+  <h2>Book Order</h2>
+  <div>
+    <form>
+      <div>
+        <label>Name</label>
+        <input type="text" name="Name" >
+      </div>
+      <div>
+        <label>Phone </label>
+        <input type="text" name="phoneNo">
+      </div>
+      <div>
+        <label>Address</label>
+        <input type="textarea" name="address">
+      </div>
+      <div>
+        <label>Loaction</label>
+        <input type="text" name="location">
+      </div>
+      <div>
+        <label>building type</label>
+        <input type="text" name="buildtype">
+      </div>
+      <div>
+        <label>Work type</label>
+        <input type="text" name="worktype">
+      </div>
+      <div>
+        <label>squarefeet</label>
+        <input type="text" name="sqfeet">
+      </div>
+      <div>
+        <input type="submit" value="Book">
+      </div>
+    </form>
+  </div>
+</body>
+</html>
